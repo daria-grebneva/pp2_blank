3dfcb3021962470226bfb0ee7602f07975d3e90e
daria-grebneva <greda2598@gmail.com>
2019-04-04 23:11:40 +0300

    [task_2] реализовала создание необходимого количества экземпляров класса CBankClient в зависимости от значения, переданного в параметре командной строки (по умолчанию - 2

--- PP2/PP2.cpp
+++ PP2/PP2.cpp
@@ -2,11 +2,15 @@
 #include "Bank.h"
 #include "BankClient.h"
 
-int main()
+int main(int argc, char *argv[])
 {
 	CBank* bank = new CBank();
-	CBankClient* client1 = bank->CreateClient();
-	CBankClient* client2 = bank->CreateClient();
+
+	int clientsNumber = (argc == 2) ? atoi(argv[1]) : 2;
+
+	for (int i = 0; i < clientsNumber; i++) {
+		CBankClient* client = bank->CreateClient();
+	}
 
 	// TODO: WaitForMultipleObjects
 	while (true)
